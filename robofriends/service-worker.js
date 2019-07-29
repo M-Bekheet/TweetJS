44ddf3a0142e5a84dea98fa76a51dd05094505cf
Lartwel <mlartwel@gmail.com>
2019-07-29 06:30:56 +0200

    Initial commit

--- robofriends/service-worker.js
+++ robofriends/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/E:/workspace/robofriends/build/index.html/precache-manifest.3c2842c9894ce841b8cb9d957c8e2c56.js"
+  "/robofriends/precache-manifest.5eb7f068787b70efba66e93ca73f6fe0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/E:/workspace/robofriends/build/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
